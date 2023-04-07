cc3801a2c784d25151f77ef161d078facd1d3901
unknown <1642264501lwy@pku.edu.cn>
2023-04-07 18:18:18 +0800

    upload Summary_Plot

--- plot_view.py
+++ plot_view.py
@@ -5,7 +5,8 @@ from PIL import Image
 from astropy.time import Time
 
 # path = os.getcwd()
-path = 'D:/SolO_epd_py/Summary_plot/summary_plot_page'
+# path = 'D:/SolO_epd_py/Summary_plot/summary_plot_page'
+path = 'D:'
 # st.markdown("# Summary Plot Overview")
 st.title('Summary Plot Overview')
 select_day = st.sidebar.date_input("Select Date", datetime.date(2022, 1, 1))
@@ -14,6 +15,7 @@ start_time = Time(str(select_day), format='isot', scale='utc')
 # end_time = Time(start_time.jd+1, format='jd', scale='utc')
 # st.markdown('## Time Range：'+start_time.isot[0:10].replace('-', '/')+' — '+end_time.isot[0:10].replace('-', '/'))
 st.markdown('## Time Range：' + start_time.isot[0:10].replace('-', '/'))
+# st.markdown('## Time Range：' + str(select_day).replace('-', '/'))
 
 st.header('1. Solar Orbiter')
 col1, col2 = st.columns(2)
